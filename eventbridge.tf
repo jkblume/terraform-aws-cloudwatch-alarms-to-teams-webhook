8c053c2f82b88c02029478642b63fb299468ea6b
Jakob Blume <jakob.blume@mailbox.org>
2025-02-20 22:52:24 +0100

    split ok and alarm rule to support separate images for each of them

--- eventbridge.tf
+++ eventbridge.tf
@@ -15,26 +15,31 @@ resource "aws_cloudwatch_event_connection" "this" {
 
 resource "aws_cloudwatch_event_api_destination" "this" {
   name                             = local.name_with_id
-  description                      = "API Destination to call Teams webhool endpoint"
+  description                      = "API Destination to call teams webhook endpoint"
   invocation_endpoint              = var.webhook_url
   http_method                      = "POST"
   invocation_rate_limit_per_second = 10
   connection_arn                   = aws_cloudwatch_event_connection.this.arn
 }
 
-resource "aws_cloudwatch_event_rule" "this" {
-  name        = local.name_with_id
-  description = "EventBridge rule to send teams notifications for alarm state changes for defined alarms"
+resource "aws_cloudwatch_event_rule" "this_ok" {
+  name        = "${local.name_with_id}-ok"
+  description = "EventBridge rule to send teams notifications for OK state changes for defined alarms"
   event_pattern = jsonencode({
     source      = ["aws.cloudwatch"]
     detail-type = ["CloudWatch Alarm State Change"]
     resources   = var.alarm_arns
+    detail = {
+      state = {
+        value = ["OK"]
+      }
+    }
   })
 }
 
-resource "aws_cloudwatch_event_target" "this" {
+resource "aws_cloudwatch_event_target" "this_ok" {
   arn      = aws_cloudwatch_event_api_destination.this.arn
-  rule     = aws_cloudwatch_event_rule.this.name
+  rule     = aws_cloudwatch_event_rule.this_ok.name
   role_arn = aws_iam_role.this.arn
 
   input_transformer {
@@ -48,6 +53,51 @@ resource "aws_cloudwatch_event_target" "this" {
       region : "$.region",
       account : "$.account"
     }
-    input_template = file("${path.module}/res/webhook_template.json")
+    input_template = templatefile(
+      "${path.module}/res/webhook_template.json",
+      {
+        imageData = filebase64("${path.module}/res/ok.png")
+      }
+    )
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "this_alarm" {
+  name        = "${local.name_with_id}-alarm"
+  description = "EventBridge rule to send teams notifications for ALARM state changes for defined alarms"
+  event_pattern = jsonencode({
+    source      = ["aws.cloudwatch"]
+    detail-type = ["CloudWatch Alarm State Change"]
+    resources   = var.alarm_arns
+    detail = {
+      state = {
+        value = ["ALARM"]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "this_alarm" {
+  arn      = aws_cloudwatch_event_api_destination.this.arn
+  rule     = aws_cloudwatch_event_rule.this_alarm.name
+  role_arn = aws_iam_role.this.arn
+
+  input_transformer {
+    input_paths = {
+      previousState : "$.detail.previousState.value",
+      previousTimestamp : "$.detail.previousState.timestamp",
+      state : "$.detail.state.value",
+      timestamp : "$.detail.state.timestamp",
+      alarmName : "$.detail.alarmName",
+      description : "$.detail.configuration.description",
+      region : "$.region",
+      account : "$.account"
+    }
+    input_template = templatefile(
+      "${path.module}/res/webhook_template.json",
+      {
+        imageData = filebase64("${path.module}/res/alarm.png")
+      }
+    )
   }
 }
--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -6,11 +6,12 @@ locals {
   region = "us-east-1"
 }
 
-module "simple_example" {  
-    source  = "jkblume/cloudwatch-alarms-to-teams-webhook/aws"
-    name = "descriptive-name" // for identifying created resources by a descriptive name
+module "simple_example" {
+    // source  = "jkblume/cloudwatch-alarms-to-teams-webhook/aws"
+    source = "../../"
+    name = "my-name" // for identifying created resources by a descriptive name
     alarm_arns = [
-        // insert alarm arns you want to monitor
+        ""
     ]
-    webhook_url = "https://insert.your.webhook.url" // checkout this: https://learn.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook
+    webhook_url = "" // checkout this: https://learn.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook
 }
